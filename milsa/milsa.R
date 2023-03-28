5b9aadcb4608dfd9a68698704ee819cc2ba2ad39
lineu96 <lialcafre@gmail.com>
2023-03-28 18:21:08 -0300

    adiciona graficos

--- milsa/milsa.R
+++ milsa/milsa.R
@@ -5,7 +5,6 @@
 ## traz no primeiro capítulo um conjunto de dados hipotético 
 ## de atributos de 36 funcionários da companhia Milsa
 
-dados$estado_civil
 #-----------------------------------------------------------------------
 
 dados <- read.csv("milsa.csv")
@@ -25,9 +24,11 @@ tabela1 <-
   add_row(estado_civil = 'Total', 
           Frequência = sum(tabela1$Frequência),
           `Freq. Relativa` = sum(tabela1$`Freq. Relativa`)
-          )
+  )
 
 names(tabela1)[1] <- "Estado civil" 
+
+#-----------------------------------------------------------------------
 
 knitr::kable(tabela1, 
              caption = "Tabela de frequências para o estado civil", 
@@ -65,6 +66,64 @@ ggplot(data=tabela1[-nrow(tabela1),],
 
 #-----------------------------------------------------------------------
 
+ggplot(data=tabela1[-nrow(tabela1),], aes(x=1, 
+                                          y=`Freq. Relativa`, 
+                                          fill = `Estado civil`
+)) + 
+  geom_col(col = 1) +
+  geom_text(aes(label = `Freq. Relativa`),
+            position = position_stack(vjust = 0.5),
+            size = 8) +
+  ylab("Proporção") +
+  xlab("") +
+  ggtitle("Gráfico")+
+  theme_classic() + theme(#legend.position = 'bottom',
+    plot.title = element_text(family = "Helvetica", 
+                              face = "bold", 
+                              size = (20),
+                              hjust = 0.5),
+    axis.title = element_text(face = "bold",
+                              size = 15),
+    axis.text.x = element_blank(),
+    legend.title = element_blank(),
+    text = element_text(size=15))  +
+  scale_fill_brewer(palette="Paired")
+
+#-----------------------------------------------------------------------
+
+tabela1 <- tabela1[-nrow(tabela1),] %>% 
+  arrange(desc(`Estado civil`)) %>%
+  mutate(ypos = cumsum(`Freq. Relativa`)- 0.5*`Freq. Relativa`)
+
+ggplot(tabela1, 
+       aes(x="", y=`Freq. Relativa`, fill=`Estado civil`)) +
+  geom_bar(stat="identity", 
+           width=1,
+           col = 1,
+           lwd = 1) +
+  coord_polar("y", start=0) +
+  theme_classic() + 
+  #theme(legend.position="none") +
+  geom_text(aes(y = ypos, label = `Freq. Relativa`), 
+            color = 1, 
+            size = 5)+
+  ylab("") +
+  xlab("") +
+  ggtitle("Gráfico")+
+  theme(legend.position = 'right',
+        plot.title = element_text(family = "Helvetica", 
+                                  face = "bold", 
+                                  size = (20),
+                                  hjust = 0.5),
+        axis.title = element_text(face = "bold",
+                                  size = 15),
+        axis.text = element_blank(),
+        legend.title = element_blank(),
+        text = element_text(size=20)) +
+  scale_fill_brewer(palette="Paired")
+
+#-----------------------------------------------------------------------
+
 tabela3 <- dados %>%
   group_by(regiao) %>%
   summarize(Frequência = n()) %>%
@@ -79,6 +138,8 @@ tabela3 <-
 
 names(tabela3)[1] <- "Região" 
 
+#-----------------------------------------------------------------------
+
 knitr::kable(tabela3, 
              caption = "Tabela de frequências para a região", 
              align ="ccc" )
@@ -115,42 +176,15 @@ ggplot(data=tabela3[-nrow(tabela3),],
 
 #-----------------------------------------------------------------------
 
-tabela3 <- dados %>%
-  group_by(regiao) %>%
-  summarize(Frequência = n()) %>%
-  mutate(`Freq. Relativa` = round(Frequência/sum(Frequência),2)) %>% 
-  arrange(desc(Frequência))
-
-tabela3 <- 
-  tabela3 %>% 
-  add_row(regiao = 'Total', 
-          Frequência = sum(tabela1$Frequência),
-          `Freq. Relativa` = sum(tabela1$`Freq. Relativa`)
-  )
-
-names(tabela3)[1] <- "Região" 
-
-knitr::kable(tabela3, 
-             caption = "Tabela de frequências para a região", 
-             align ="ccc" )
-
-#-----------------------------------------------------------------------
-
-ggplot(data=tabela3[-nrow(tabela3),], 
-       aes(x=Região, 
-           y=Frequência#, 
-           #fill = Niveis
-       )) + 
-  geom_bar(stat="identity", 
-           col=1,
-           lwd=1)+
-  ylim(c(0, 
-         (max(tabela3[-nrow(tabela3),]$Frequência)+ (max(tabela3[-nrow(tabela3),]$Frequência)*0.2))))+
-  geom_text(aes(label=Frequência), 
-            vjust=-1, 
-            color=1, 
-            size=8)+
-  ylab("Frequência") +
+ggplot(data=tabela3[-nrow(tabela3),], aes(x=1, 
+                                          y=`Freq. Relativa`, 
+                                          fill = Região
+)) + 
+  geom_col(col = 1) +
+  geom_text(aes(label = `Freq. Relativa`),
+            position = position_stack(vjust = 0.5),
+            size = 8) +
+  ylab("Proporção") +
   xlab("") +
   ggtitle("Gráfico")+
   theme_classic() + theme(#legend.position = 'bottom',
@@ -160,9 +194,43 @@ ggplot(data=tabela3[-nrow(tabela3),],
                               hjust = 0.5),
     axis.title = element_text(face = "bold",
                               size = 15),
-    #axis.text = element_blank(),
+    axis.text.x = element_blank(),
     legend.title = element_blank(),
-    text = element_text(size=15)) 
+    text = element_text(size=15))  +
+  scale_fill_brewer(palette="Paired")
+
+#-----------------------------------------------------------------------
+
+tabela3 <- tabela3[-nrow(tabela3),] %>% 
+  arrange(desc(Região)) %>%
+  mutate(ypos = cumsum(`Freq. Relativa`)- 0.5*`Freq. Relativa`)
+
+ggplot(tabela3, 
+       aes(x="", y=`Freq. Relativa`, fill=Região)) +
+  geom_bar(stat="identity", 
+           width=1,
+           col = 1,
+           lwd = 1) +
+  coord_polar("y", start=0) +
+  theme_classic() + 
+  #theme(legend.position="none") +
+  geom_text(aes(y = ypos, label = `Freq. Relativa`), 
+            color = 1, 
+            size = 5)+
+  ylab("") +
+  xlab("") +
+  ggtitle("Gráfico")+
+  theme(legend.position = 'right',
+        plot.title = element_text(family = "Helvetica", 
+                                  face = "bold", 
+                                  size = (20),
+                                  hjust = 0.5),
+        axis.title = element_text(face = "bold",
+                                  size = 15),
+        axis.text = element_blank(),
+        legend.title = element_blank(),
+        text = element_text(size=20)) +
+  scale_fill_brewer(palette="Paired")
 
 #-----------------------------------------------------------------------
 
@@ -179,6 +247,8 @@ tabela2 <-
   )
 
 names(tabela2)[1] <- "Instrução" 
+
+#-----------------------------------------------------------------------
 
 knitr::kable(tabela2, 
              caption = "Tabela de frequências para o grau de instrução", 
@@ -216,6 +286,60 @@ ggplot(data=tabela2[-nrow(tabela2),],
 
 #-----------------------------------------------------------------------
 
+ggplot(data=tabela2[-nrow(tabela2),], aes(x=1, 
+                                          y=`Freq. Relativa`, 
+                                          fill = Instrução
+)) + 
+  geom_col(col = 1) +
+  geom_text(aes(label = `Freq. Relativa`),
+            position = position_stack(vjust = 0.5),
+            size = 8) +
+  ylab("Proporção") +
+  xlab("") +
+  ggtitle("Gráfico")+
+  theme_classic() + theme(#legend.position = 'bottom',
+    plot.title = element_text(family = "Helvetica", 
+                              face = "bold", 
+                              size = (20),
+                              hjust = 0.5),
+    axis.title = element_text(face = "bold",
+                              size = 15),
+    axis.text.x = element_blank(),
+    legend.title = element_blank(),
+    text = element_text(size=15))  +
+  scale_fill_brewer(palette="Paired")
 
+#-----------------------------------------------------------------------
+
+tabela2 <- tabela2[-nrow(tabela2),] %>% 
+  arrange(desc(Instrução)) %>%
+  mutate(ypos = cumsum(`Freq. Relativa`)- 0.5*`Freq. Relativa`)
+
+ggplot(tabela2, 
+       aes(x="", y=`Freq. Relativa`, fill=Instrução)) +
+  geom_bar(stat="identity", 
+           width=1,
+           col = 1,
+           lwd = 1) +
+  coord_polar("y", start=0) +
+  theme_classic() + 
+  #theme(legend.position="none") +
+  geom_text(aes(y = ypos, label = `Freq. Relativa`), 
+            color = 1, 
+            size = 5)+
+  ylab("") +
+  xlab("") +
+  ggtitle("Gráfico")+
+  theme(legend.position = 'right',
+        plot.title = element_text(family = "Helvetica", 
+                                  face = "bold", 
+                                  size = (20),
+                                  hjust = 0.5),
+        axis.title = element_text(face = "bold",
+                                  size = 15),
+        axis.text = element_blank(),
+        legend.title = element_blank(),
+        text = element_text(size=20)) +
+  scale_fill_brewer(palette="Paired")
 
 #-----------------------------------------------------------------------
